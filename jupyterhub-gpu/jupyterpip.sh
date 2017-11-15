1d525674114c6a4d4b0db4b723eea5793361541c
ren-hoek <ren-hoek@users.noreply.github.com>
2017-11-15 16:24:01 +0000

    added basic gpu support and pyenchant

---
+++ jupyterhub-gpu/jupyterpip.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+docker service create \
+	--mount type=bind,src=/var/run/docker.sock,dst=/var/run/docker.sock \
+	--mount type=bind,src=/usr/bin/docker,dst=/usr/bin/docker \
+	--mount type=bind,src=/etc/passwd,dst=/etc/passwd \
+	--mount type=bind,src=/etc/shadow,dst=/etc/shadow \
+	--mount type=bind,src=/etc/group,dst=/etc/group \
+	--mount type=bind,src=/etc/gshadow,dst=/etc/gshadow \
+	--name jupyterpip \
+	--hostname jupyter \
+	--publish 8000:8000 \
+	--network jupyterhub analysis/jupyterpip-gpu
+
